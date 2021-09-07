95e3367ad61b1d6dad6ac17c6659a261f068dc55
alek101 <47416711+alek101@users.noreply.github.com>
2021-09-07 22:13:44 +0200

    trying to make code look little more respectfully

--- resources/js/components/Favorites.vue
+++ resources/js/components/Favorites.vue
@@ -26,73 +26,65 @@ export default {
     name: 'Favorites',
     data(){
         return {
-            tradingPairs: [
-                   
-                ],
+            tradingPairs: [],
             isLoged: false,
             favorites: {}, 
             pairs: [],     
         }
     },
-    
-    
-     methods: {
-         handlePercentage(num){
-             num = num  *100;
-             return num>0? '+'+num.toFixed(2)+'%':num.toFixed(2)+'%';
-         }
-        },
-        async beforeMount() {
-            const res = await fetch('/api/status');
-            const resr = await res.json();
-            if(resr.code){
-                alert(r2r.error_description);
-            }
-            else {
-               this.isLoged=resr.isLoged;
-            this.favorites=resr.favorites;
-            this.pairs = Object.keys(this.favorites);
-            const that=this;
-             for(let i=0; i<that.pairs.length; i++){
-                
-                let np = {
-                            name: that.pairs[i],
-                            last: null,
-                            change: null,
-                            changePercentage: null,
-                            high: null,
-                            low: null
-                            };
-                that.tradingPairs.push(np);
-               let ws = new WebSocket('wss://api-pub.bitfinex.com/ws/2');
-
-                ws.onopen = function(){
-                    ws.send(JSON.stringify({
-                         event: 'subscribe', 
-                        channel: 'ticker', 
-                        symbol: 't'+that.pairs[i], 
-                    }));
-        
-                }
-
-                ws.onmessage = function(msg){
-                        let response = JSON.parse(msg.data);
-                        console.log(that.pairs[i], response); 
-                        if(response[1] && response[1] != "hb")
-                        {
-                           
-                            that.tradingPairs[i].last = (response[1][6]).toFixed(2);
-                            that.tradingPairs[i].change = (response[1][4]).toFixed(2);
-                            that.tradingPairs[i].changePercentage = that.handlePercentage(response[1][5]);
-                            that.tradingPairs[i].high = (response[1][8]).toFixed(2);
-                            that.tradingPairs[i].low = (response[1][9]).toFixed(2);
-                        }
-                        
-                    }
-            } 
-            }
+    methods: {
+        handlePercentage(num){
+            num = num  *100;
+            return num>0? '+'+num.toFixed(2)+'%':num.toFixed(2)+'%';
+        }
+    },
+    async beforeMount() {
+        const res = await fetch('/api/status');
+        const resr = await res.json();
+        if(resr.code){
+            alert(r2r.error_description);
+        }
+        else {
+            this.isLoged=resr.isLoged;
+        this.favorites=resr.favorites;
+        this.pairs = Object.keys(this.favorites);
+        const that=this;
+            for(let i=0; i<that.pairs.length; i++){
             
-        } ,
+            let np = {
+                    name: that.pairs[i],
+                    last: null,
+                    change: null,
+                    changePercentage: null,
+                    high: null,
+                    low: null
+                };
+            that.tradingPairs.push(np);
+            let ws = new WebSocket('wss://api-pub.bitfinex.com/ws/2');
+
+            ws.onopen = function(){
+                ws.send(JSON.stringify({
+                        event: 'subscribe', 
+                    channel: 'ticker', 
+                    symbol: 't'+that.pairs[i], 
+                }));
+            }
+
+            ws.onmessage = function(msg){
+                    let response = JSON.parse(msg.data);
+                    console.log(that.pairs[i], response); 
+                    if(response[1] && response[1] != "hb")
+                    {
+                        that.tradingPairs[i].last = (response[1][6]).toFixed(2);
+                        that.tradingPairs[i].change = (response[1][4]).toFixed(2);
+                        that.tradingPairs[i].changePercentage = that.handlePercentage(response[1][5]);
+                        that.tradingPairs[i].high = (response[1][8]).toFixed(2);
+                        that.tradingPairs[i].low = (response[1][9]).toFixed(2);
+                    }
+                }
+            } 
+        } 
+    } ,
 
 }
 </script>
--- resources/js/components/HomePage.vue
+++ resources/js/components/HomePage.vue
@@ -27,90 +27,82 @@
 export default {
     name: 'HomePage',
      data() {
-            return {
-                tradingPairs: [
-                ],  
-                pairs: [
-
-                ]            
-            }
-        },
-        methods: {
-         handlePercentage(num){
-             num = num  *100;
-             return num>0? '+'+num.toFixed(2)+'%':num.toFixed(2)+'%';
-         },
-        getTopFive(array) {
-        const array2 = array.slice(0,5);
-        const result = [];
-        for(let mem of array2){
-            result.push(mem.toUpperCase());
+        return {
+            tradingPairs: [],  
+            pairs: []            
         }
-        return result;
-        }
+    },
+    methods: {
+        handlePercentage(num){
+            num = num  *100;
+            return num>0? '+'+num.toFixed(2)+'%':num.toFixed(2)+'%';
         },
-        async beforeMount() {
-            const res = await fetch('/api/getSymbols',{
-                method: 'GET',
-                headers: {
-                    'Access-Control-Allow-Origin': '*',
-                    'Content-Type': 'application/json',
-                },
-                mode: 'cors'
-            })
-                const resr= await res.json();
-                
-
-             if(resr.code) {
-                alert(resr.error_description);
-            }
-            else
-            {
-             this.pairs = this.getTopFive(resr);
-                    const that = this;
-                    for(let i=0; i<that.pairs.length; i++){
-                        
-                        let np = {
-                                    name: that.pairs[i],
-                                    last: null,
-                                    change: null,
-                                    changePercentage: null,
-                                    high: null,
-                                    low: null
-                                    };
-                        that.tradingPairs.push(np);
-                    let ws = new WebSocket('wss://api-pub.bitfinex.com/ws/2');
-
-                        ws.onopen = function(){
-                            ws.send(JSON.stringify({
-                                event: 'subscribe', 
-                                channel: 'ticker', 
-                                symbol: 't'+that.pairs[i],
-                            }));
-                
-                        }
-
-                        ws.onmessage = function(msg){
-                                let response = JSON.parse(msg.data);
-                                console.log(that.pairs[i], response); 
-                                if(response[1] && response[1] != "hb")
-                                {
-                                
-                                    that.tradingPairs[i].last = (response[1][6]).toFixed(2);
-                                    that.tradingPairs[i].change = (response[1][4]).toFixed(2);
-                                    that.tradingPairs[i].changePercentage = that.handlePercentage(response[1][5]);
-                                    that.tradingPairs[i].high = (response[1][8]).toFixed(2);
-                                    that.tradingPairs[i].low = (response[1][9]).toFixed(2);
-                                }
-                                
-                            }
-                    }
-                } 
-            }
- 
+    getTopFive(array) {
+    const array2 = array.slice(0,5);
+    const result = [];
+    for(let mem of array2){
+        result.push(mem.toUpperCase());
+    }
+    return result;
+    }
+    },
+    async beforeMount() {
+        const res = await fetch('/api/getSymbols',{
+            method: 'GET',
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json',
+            },
+            mode: 'cors'
+        })
+            const resr= await res.json();
             
+
+            if(resr.code) {
+            alert(resr.error_description);
+        }
+        else
+        {
+            this.pairs = this.getTopFive(resr);
+                const that = this;
+                for(let i=0; i<that.pairs.length; i++){
+                    
+                    let np = {
+                        name: that.pairs[i],
+                        last: null,
+                        change: null,
+                        changePercentage: null,
+                        high: null,
+                        low: null
+                        };
+                    that.tradingPairs.push(np);
+                let ws = new WebSocket('wss://api-pub.bitfinex.com/ws/2');
+
+                ws.onopen = function(){
+                    ws.send(JSON.stringify({
+                        event: 'subscribe', 
+                        channel: 'ticker', 
+                        symbol: 't'+that.pairs[i],
+                    }));
         
-       
+                }
+
+                ws.onmessage = function(msg){
+                        let response = JSON.parse(msg.data);
+                        console.log(that.pairs[i], response); 
+                        if(response[1] && response[1] != "hb")
+                        {
+                        that.tradingPairs[i].last = (response[1][6]).toFixed(2);
+                        that.tradingPairs[i].change = (response[1][4]).toFixed(2);
+                        that.tradingPairs[i].changePercentage = that.handlePercentage(response[1][5]);
+                        that.tradingPairs[i].high = (response[1][8]).toFixed(2);
+                        that.tradingPairs[i].low = (response[1][9]).toFixed(2);
+                        }
+                        
+                    }
+                }
+            } 
+        }     
 }
 </script>
 
